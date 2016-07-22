5e9b981dcc0a314c7248cca6c3f2de38ca5846fe
kolektiv <andrew@xyncro.com>
2016-07-22 01:33:04 +0100

    basic quick example being ported and tidied

---
+++ src/TodoBackend/Program.fs
@@ -0,0 +1,30 @@
+﻿module TodoBackend.Program
+
+open System
+open Freya.Core
+
+(* Katana
+
+   Katana (Owin Self Hosting) expects us to expose a type with a specific
+   method. Freya lets us do see easily, the OwinAppFunc module providing
+   functions to turn any Freya<'a> function in to a suitable value for
+   OWIN compatible hosts such as Katana. *)
+
+type TodoBackend () =
+    member __.Configuration () =
+        OwinAppFunc.ofFreya (Api.todoRoutes)
+
+(* Main
+   A very simple program, simply a console app, with a blocking read from
+   the console to keep our server from shutting down immediately. Though
+   we are self hosting here as a console application, the same application
+   should be easily transferrable to any OWIN compatible server, including
+   IIS. *)
+
+open Microsoft.Owin.Hosting
+
+[<EntryPoint>]
+let main _ = 
+    let _ = WebApp.Start<TodoBackend> ("http://localhost:7000")
+    let _ = Console.ReadLine ()
+    0
